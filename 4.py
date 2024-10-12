da196035eebcb505221784d504e44e048b881d93
Pradeep B <deeppradeep1998@gmail.com>
2024-10-12 21:25:19 +0530

    first commit

---
+++ 4.py
@@ -0,0 +1,9 @@
+d={'mom':1,'dad':2,'son':3}
+
+def modify_dict(d):
+    d={'grandma':4,'grandpa':5}
+    
+
+modify_dict(d)
+print(d)
+#{'mom': 1, 'dad': 2, 'son': 3}
---
+++ 5.py
@@ -0,0 +1,8 @@
+d = {'mom': 1, 'dad': 2, 'son': 3}
+def modify_dict(d):
+        d['grandma']=4
+        d['grandpa'] = 5
+        return d
+
+d = modify_dict(d)
+print(d)
